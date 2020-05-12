14c941ea2447ec84591d219f9561a48f388e7bde
vnorr1 <vnorr1@users.noreply.github.com>
2020-05-12 19:38:10 +0200

    Created dynamic image/button

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ let todaysDate = new Date();
 let dateElement = document.getElementById("date");
 
 dateElement.innerHTML = todaysDate.toISOString().substring(0, 10);
+
+function myFunction() {
+  let x = document.getElementById("button");
+  let y = document.getElementById("cake");
+  if (x.innerHTML === "Light the candles!") {
+    x.innerHTML = "Blow out candles";
+    y.src = "images/cake-icon.png";
+  } else {
+    x.innerHTML = "Light the candles!";
+    y.src = "images/cake-icon-off.png";
+  }
+}
