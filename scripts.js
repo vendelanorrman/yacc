2d0d730b6ef005981cbbff67a426263f71923cae
vnorr1 <vnorr1@users.noreply.github.com>
2020-06-03 22:24:14 +0200

    Added fikaDays API

--- scripts.js
+++ scripts.js
@@ -15,3 +15,37 @@ function myFunction() {
     y.src = "images/cake-icon-off.png";
   }
 }
+
+function showFikaDays(jsonFikaDays) {
+  const fikaDays = jsonFikaDays["data"];
+  let fikaDaysElement = document.getElementById("fikaDaysContainer");
+  for (let i = 0; i < fikaDays.length; i++) {
+    const fikaDayElement = document.createElement("div");
+    const fikaDayNameElement = document.createElement("span");
+
+    fikaDayElement.id = "fika-day-" + i;
+    fikaDayNameElement.innerHTML = fikaDays[i].name;
+    fikaDayElement.appendChild(fikaDayNameElement);
+    fikaDaysElement.appendChild(fikaDayElement);
+    // const myH2 = document.createElement("h2");
+    // myH2.textContent = heroes[i].name;
+    // myPara1.textContent = "Secret identity: " + heroes[i].secretIdentity;
+    // const superPowers = heroes[i].powers;
+    // myArticle.appendChild(myList);
+    // section.appendChild(myArticle);
+  }
+  console.log(fikaDays);
+}
+
+let requestURL = "https://diydata.dev/api/swedishfikadays/2020/";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+// console.log(request.response);
+
+request.onload = function () {
+  const fikaDays = request.response;
+  console.log(fikaDays);
+  showFikaDays(fikaDays);
+};
